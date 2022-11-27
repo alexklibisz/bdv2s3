fcf6e50a2a1f5643715a1907de07d85bcdbe78f6
Alex Klibisz <aklibisz@gmail.com>
2022-11-27 10:29:00 -0500

    WIP

--- test/happy-path/local.sh
+++ test/happy-path/local.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 set -e
 
-mkdir -p data/container1/d1
-echo "container1/d1/f1" > data/container1/d1/f1.txt
-echo "container1/d1/f2" > data/container1/d1/f2.txt
+mkdir -p backup/container1/d1
+echo "container1/d1/f1" > backup/container1/d1/f1.txt
+echo "container1/d1/f2" > backup/container1/d1/f2.txt
 
-mkdir -p data/container1/d2
-echo "container1/d2/f1" > data/container1/d2/f1.txt
-echo "container1/d2/f2" > data/container1/d2/f2.txt
+mkdir -p backup/container1/d2
+echo "container1/d2/f1" > backup/container1/d2/f1.txt
+echo "container1/d2/f2" > backup/container1/d2/f2.txt
 
-mkdir -p data/container2/d1
-echo "container2/d1/f1" > data/container2/d1/f1.txt
-echo "container2/d1/f2" > data/container2/d1/f2.txt
+mkdir -p backup/container2/d1
+echo "container2/d1/f1" > backup/container2/d1/f1.txt
+echo "container2/d1/f2" > backup/container2/d1/f2.txt
 
-mkdir -p data/container2/d2
-echo "container2/d2/f1" > data/container2/d2/f1.txt
-echo "container2/d2/f2" > data/container2/d2/f2.txt
+mkdir -p backup/container2/d2
+echo "container2/d2/f1" > backup/container2/d2/f1.txt
+echo "container2/d2/f2" > backup/container2/d2/f2.txt
 
 tar cvf backup.tar --sort=name data
 md5sum backup.tar
