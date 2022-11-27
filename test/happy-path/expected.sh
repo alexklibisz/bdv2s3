a4cef71082f4b53701aea2a48a74015b1e9e2dc0
Alex Klibisz <aklibisz@gmail.com>
2022-11-27 12:00:14 -0500

    WIP

--- test/happy-path/expected.sh
+++ test/happy-path/expected.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 sleep 3
-tar cvf /out/expected.tar --sort=name /backup
+tar cvf /out/expected.tar --sort=name backup
 md5sum /out/expected.tar
 gzip -n -f /out/expected.tar
 md5sum /out/expected.tar.gz
