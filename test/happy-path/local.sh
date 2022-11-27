20060b41b43c9f5d90983b665da73b06560ae30b
Alex Klibisz <aklibisz@gmail.com>
2022-11-26 23:47:54 -0500

    WIP

--- test/happy-path/local.sh
+++ test/happy-path/local.sh
@@ -6,5 +6,5 @@ echo "d1/f1" > backup/container1/d1/f1.txt
 echo "d1/f2" > backup/container1/d1/f2.txt
 echo "d2/f1" > backup/container2/d2/f1.txt
 echo "d2/f2" > backup/container2/d2/f2.txt
-tar cvzf --sort=name /tmp/local.tar.gz backup
+tar --sort=name cvzf /tmp/local.tar.gz backup
 md5sum /tmp/local.tar.gz
