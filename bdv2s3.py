85e3dc93cc6c62709d4534b2c202c5e23c2d25ee
Alex Klibisz <aklibisz@gmail.com>
2022-11-26 20:45:06 -0500

    WIP

--- bdv2s3.py
+++ bdv2s3.py
@@ -9,6 +9,7 @@ assert 'AWS_SECRET_ACCESS_KEY' in os.environ, "Expected env var AWS_SECRET_ACCES
 assert 'AWS_DEFAULT_REGION' in os.environ, "Expected env var AWS_DEFAULT_REGION"
 assert 'AWS_S3_BUCKET' in os.environ, "Expected env var AWS_S3_BUCKET"
 assert 'HEARTBEAT_URL' in os.environ, "Expected env var HEARTBEAT_URL"
+assert 'INITIAL_SLEEP_SECONDS' in os.environ, "Expected env var INITIAL_SLEEP_SECONDS"
 assert 'SLEEP_SECONDS' in os.environ, "Expected env var SLEEP_SECONDS"
 assert 'FILENAME_PREFIX' in os.environ, "Expected env var FILENAME_PREFIX"
 assert os.path.exists('/backup') and os.path.isdir('/backup'), "Expected directory at /backup"
@@ -16,10 +17,12 @@ assert os.path.exists('/backup') and os.path.isdir('/backup'), "Expected directo
 aws_s3_bucket = os.environ['AWS_S3_BUCKET']
 heartbeaat_url = os.environ['HEARTBEAT_URL']
 filename_prefix = os.environ['FILENAME_PREFIX']
+initial_sleep_seconds = int(os.environ['INITIAL_SLEEP_SECONDS'])
 sleep_seconds = int(os.environ['SLEEP_SECONDS'])
 
 client = boto3.client('s3')
 
+time.sleep(initial_sleep_seconds)
 while True:
     backup_tarfile = f'/tmp/{filename_prefix}-{int(time.time())}.tar.gz'
     print(f"Creating new backup at [{backup_tarfile}]")
