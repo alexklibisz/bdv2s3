4f99dcd7ff976b36eae5bf3ee8b12e4493c3b1b3
Alex Klibisz <aklibisz@gmail.com>
2022-11-27 11:22:59 -0500

    WIP

--- backup.sh
+++ backup.sh
@@ -3,9 +3,10 @@ set -e
 /bin/sh /checkenv.sh
 AWS_S3_KEY="$AWS_S3_KEY_PREFIX-$(date +%Y%m%d%H%M%S).tar.gz"
 echo "Creating new backup with key [${AWS_S3_KEY}]"
-tar cvf /tmp/backup.tar --sort=name backup
-md5sum /tmp/backup.tar
-gzip -f -n /tmp/backup.tar
-md5sum /tmp/backup.tar.gz
-aws s3 cp /tmp/backup.tar.gz s3://$AWS_S3_BUCKET/$AWS_S3_KEY
+tar cvf backup.tar --sort=name ./backup
+md5sum backup.tar
+gzip -f -n backup.tar
+md5sum backup.tar.gz
+aws s3 cp backup.tar.gz s3://$AWS_S3_BUCKET/$AWS_S3_KEY
+rm backup.tar.gz
 echo "Completed backup"
