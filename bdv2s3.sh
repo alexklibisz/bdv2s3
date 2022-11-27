debf4d67a442b31671300e754c7b9123e6a4713a
Alex Klibisz <aklibisz@gmail.com>
2022-11-27 00:19:56 -0500

    WIP

--- bdv2s3.sh
+++ bdv2s3.sh
@@ -47,6 +47,7 @@ do
     AWS_S3_KEY="$AWS_S3_KEY_PREFIX-$(date +%Y%m%d%H%M%S).tar.gz"
     echo "Creating new backup with key [${AWS_S3_KEY}]"
     tar cvzf /tmp/backup.tar.gz --sort=name backup
+    md5sum /tmp/backup.tar.gz
     aws s3 cp /tmp/backup.tar.gz s3://$AWS_S3_BUCKET/$AWS_S3_KEY
     echo "Completed backup. Sleeping [$SLEEP_SECONDS] seconds before next backup."
     sleep $SLEEP_SECONDS
