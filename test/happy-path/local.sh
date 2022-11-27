55d4e3d816b76c3fd4afbade4c81aa71d39e3be2
Alex Klibisz <aklibisz@gmail.com>
2022-11-26 20:11:54 -0500

    WIP

--- test/happy-path/local.sh
+++ test/happy-path/local.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 cd /tmp
-mkdir -p backup/d1 backup/d2
-echo "d1/f1" > backup/d1/f1.txt
-echo "d1/f2" > backup/d1/f2.txt
-echo "d2/f1" > backup/d2/f1.txt
-echo "d2/f2" > backup/d2/f2.txt
+mkdir -p backup/container1/d1 backup/container2/d2
+echo "d1/f1" > backup/container1/d1/f1.txt
+echo "d1/f2" > backup/container1/d1/f2.txt
+echo "d2/f1" > backup/container2/d2/f1.txt
+echo "d2/f2" > backup/container2/d2/f2.txt
 tar cvzf /tmp/local.tar.gz backup
 md5sum /tmp/local.tar.gz
