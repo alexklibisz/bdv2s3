eee6b5282bd2220273dc464fb2ca8cc7e53da596
Alex Klibisz <aklibisz@gmail.com>
2022-11-27 00:15:33 -0500

    WIP

---
+++ test/happy-path/container1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+mkdir -p /data/d1
+echo "d1/f1" > /data/d1/f1.txt
+echo "d1/f2" > /data/d1/f2.txt
---
+++ test/happy-path/container2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+mkdir -p /data/d2
+echo "d2/f1" > /data/d2/f1.txt
+echo "d2/f2" > /data/d2/f2.txt
