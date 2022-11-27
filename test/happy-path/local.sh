cde716d650856ff993c5c4f0e791821a3cef5ef4
Alex Klibisz <aklibisz@gmail.com>
2022-11-27 10:24:02 -0500

    WIP

---
+++ test/happy-path/local.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+mkdir -p data/container1/d1
+echo "container1/d1/f1" > data/container1/d1/f1.txt
+echo "container1/d1/f2" > data/container1/d1/f2.txt
+
+mkdir -p data/container1/d2
+echo "container1/d2/f1" > data/container1/d2/f1.txt
+echo "container1/d2/f2" > data/container1/d2/f2.txt
+
+mkdir -p data/container2/d1
+echo "container2/d1/f1" > data/container2/d1/f1.txt
+echo "container2/d1/f2" > data/container2/d1/f2.txt
+
+mkdir -p data/container2/d2
+echo "container2/d2/f1" > data/container2/d2/f1.txt
+echo "container2/d2/f2" > data/container2/d2/f2.txt
+
+tar cvf backup.tar --sort=name data
+md5sum backup.tar
+gzip -f -n backup.tar
+md5sum backup.tar.gz
