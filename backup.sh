4e9af43af09fe1a75fd2962e50b60c9a5d57a137
Alex Klibisz <aklibisz@gmail.com>
2022-11-27 19:24:42 -0600

    WIP

--- backup.sh
+++ backup.sh
@@ -2,10 +2,15 @@
 set -e
 /bin/sh /checkenv.sh
 
-docker ps --format "{{.ID}}" --filter "label=bdv2s3.stop-during-backup=true"
-
 AWS_S3_KEY="$AWS_S3_KEY_PREFIX$(date +%Y%m%d%H%M%S).tar.gz"
 echo "Creating new backup with key [${AWS_S3_KEY}]"
+
+echo "Stopping any containers with label bdv2s3.stop-during-backup=true"
+for container in $(docker ps --format "{{.ID}}" --filter "label=bdv2s3.stop-during-backup=true")
+do
+    echo $container
+done
+
 tar cvf backup.tar --sort=name ./backup
 md5sum backup.tar
 gzip -f -n backup.tar
