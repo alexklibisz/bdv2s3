3fe5a7c7f8e7aea3b019b297b1d7657561d5bb27
Alex Klibisz <aklibisz@gmail.com>
2022-11-27 20:34:36 -0600

    WIP

--- test/postgres/insert-data.sh
+++ test/postgres/insert-data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+pg_isready -h $PGHOST -p $PGPORT || sleep 5
+pg_isready -h $PGHOST -p $PGPORT
 time psql -v ON_ERROR_STOP=1 <<END_OF_SQL
 drop table if exists random_data;
 create table random_data (
@@ -9,7 +11,7 @@ create table random_data (
 );
 insert into random_data(int, str)
 select (random() * 1000000)::int, md5(random()::text)
-from generate_series(1, 1000);
+from generate_series(1, 100000);
 select * from random_data limit 10;
 select count(*) from random_data;
 END_OF_SQL
--- test/postgres/select-data.sh
+++ test/postgres/select-data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+pg_isready -h $PGHOST -p $PGPORT || sleep 5
+pg_isready -h $PGHOST -p $PGPORT
 psql -v ON_ERROR_STOP=1 <<END_OF_SQL
 select * from random_data order by id;
 END_OF_SQL
