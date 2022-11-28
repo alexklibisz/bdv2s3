3f191183d0e3f3bf744613c564503d9105efcf23
Alex Klibisz <aklibisz@gmail.com>
2022-11-27 20:38:56 -0600

    WIP

--- test/postgres/insert-data.sh
+++ test/postgres/insert-data.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
-pg_isready -h $PGHOST -p $PGPORT || sleep 5
-pg_isready -h $PGHOST -p $PGPORT
+pg_isready -h $PGHOST -p $PGPORT -t 5
 time psql -v ON_ERROR_STOP=1 <<END_OF_SQL
 drop table if exists random_data;
 create table random_data (
--- test/postgres/select-data.sh
+++ test/postgres/select-data.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
-pg_isready -h $PGHOST -p $PGPORT || sleep 5
-pg_isready -h $PGHOST -p $PGPORT
+pg_isready -h $PGHOST -p $PGPORT -t 5
 psql -v ON_ERROR_STOP=1 <<END_OF_SQL
 select * from random_data order by id;
 END_OF_SQL
